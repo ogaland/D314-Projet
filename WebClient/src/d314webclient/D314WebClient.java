a04b657adc18791c51d5e248763a7ff4e3e66afa
ogaland <ogaland@syder.fr>
2016-10-27 22:15:08 +0200

    Web Service

---
+++ WebClient/src/d314webclient/D314WebClient.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package d314webclient;
+
+/**
+ *
+ * @author olivier
+ */
+public class D314WebClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
